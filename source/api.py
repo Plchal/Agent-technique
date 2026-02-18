acf5e131ad1813a3fe9112f6bdedb56c16e80c7a
Plchal <pchalmin@student.42angouleme.fr>
2026-02-18 18:09:28 +0100

    ADD(WebPage): add html and css fille for my web page

--- source/api.py
+++ source/api.py
@@ -25,11 +25,16 @@ async def upload_file(file: UploadFile):
         ingester.ingest_pdf(file_location)
         return {"message": f"{file.filename} saved at {file_location}"}
     except Exception as e:
-        raise HTTPException(status_code=500, detail="Echec  de l'ingestion.")
+        raise HTTPException(status_code=500, detail="Ingestion failure.")
 
 @app.post("/ask")
 async def chat_with_your_doc(request: QuestionRequest):
-    answer = rag_engine.ask_doc(request.question)
-    if not answer:
-        raise HTTPException(status_code=500, detail="Error with answer generation !")
-    return {"question": request.question, "answer": answer}
+    try:
+        answer = rag_engine.ask_doc(request.question)
+        if not answer:
+            raise HTTPException(status_code=404, detail="Error with answer generation.")
+        return {"question": request.question, "answer": answer}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Error with RAG engine.")
+        
+            
